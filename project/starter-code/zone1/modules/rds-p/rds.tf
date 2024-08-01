7e96e35b6428832c937bf309ca4e9aa3bd133414
Mbaoma <mmaryraphaella@gmail.com>
2024-08-01 20:13:47 +0100

    updated solutions file

--- project/starter-code/zone1/modules/rds-p/rds.tf
+++ project/starter-code/zone1/modules/rds-p/rds.tf
@@ -11,54 +11,125 @@
 # }
 
 
+# resource "aws_rds_cluster_parameter_group" "cluster_pg" {
+#   name   = "udacity-pg-p"
+#   family = "aurora5.6"
+
+#   parameter {
+#     name  = "binlog_format"    
+#     value = "MIXED"
+#     apply_method = "pending-reboot"
+#   }
+
+#   parameter {
+#     name = "log_bin_trust_function_creators"
+#     value = 1
+#     apply_method = "pending-reboot"
+#   }
+# }
+
+# resource "aws_db_subnet_group" "udacity_db_subnet_group" {
+#   name       = "udacity_db_subnet_group"
+#   subnet_ids = var.private_subnet_ids
+
+# }
+# resource "aws_rds_cluster" "udacity_cluster" {
+#   cluster_identifier       = "udacity-db-cluster"
+#   availability_zones       = ["us-east-2a", "us-east-2b"]
+#   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg.name
+#   database_name            = "udacityc2"
+#   master_username          = "udacity"
+#   master_password          = "MyUdacityPassword"
+#   vpc_security_group_ids   = [aws_security_group.db_sg_1.id]
+#   db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
+#   engine_mode              = "provisioned"
+#   engine_version           = "5.6.mysql_aurora.1.19.1" 
+#   skip_final_snapshot      = true
+#   storage_encrypted        = false
+#   depends_on = [aws_rds_cluster_parameter_group.cluster_pg]
+# }
+
+# output "db_cluster_arn" {
+#   value = aws_rds_cluster.udacity_cluster.arn
+# }
+
+# output "db_instance_arn" {
+#   value = aws_rds_cluster_instance.udacity_instance[0].arn
+# }
+
+# resource "aws_rds_cluster_instance" "udacity_instance" {
+#   count                = 1
+#   identifier           = "udacity-db-instance-${count.index}"
+#   cluster_identifier   = aws_rds_cluster.udacity_cluster.id
+#   instance_class       = "db.t2.small"
+#   db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
+# }
+
+# resource "aws_security_group" "db_sg_1" {
+#   name   = "udacity-db-sg"
+#   vpc_id =  var.vpc_id
+
+#   ingress {
+#     from_port   = 3306
+#     protocol    = "TCP"
+#     to_port     = 3306
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 3306
+#     protocol    = "TCP"
+#     to_port     = 3306
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
 resource "aws_rds_cluster_parameter_group" "cluster_pg" {
   name   = "udacity-pg-p"
   family = "aurora5.6"
 
   parameter {
-    name  = "binlog_format"    
+    name  = "binlog_format"
     value = "MIXED"
     apply_method = "pending-reboot"
   }
 
   parameter {
-    name = "log_bin_trust_function_creators"
-    value = 1
+    name  = "log_bin_trust_function_creators"
+    value = "1"
     apply_method = "pending-reboot"
   }
 }
 
 resource "aws_db_subnet_group" "udacity_db_subnet_group" {
   name       = "udacity_db_subnet_group"
-  subnet_ids = var.private_subnet_ids
-
+  subnet_ids = module.vpc.private_subnet_ids
 }
+
 resource "aws_rds_cluster" "udacity_cluster" {
-  cluster_identifier       = "udacity-db-cluster"
-  availability_zones       = ["us-east-2a", "us-east-2b"]
+  cluster_identifier            = "udacity-db-cluster"
+  availability_zones            = ["us-east-2a", "us-east-2b"]
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_pg.name
-  database_name            = "udacityc2"
-  master_username          = "udacity"
-  master_password          = "MyUdacityPassword"
-  vpc_security_group_ids   = [aws_security_group.db_sg_1.id]
-  db_subnet_group_name     = aws_db_subnet_group.udacity_db_subnet_group.name
-  engine_mode              = "provisioned"
-  engine_version           = "5.6.mysql_aurora.1.19.1" 
-  skip_final_snapshot      = true
-  storage_encrypted        = false
-  depends_on = [aws_rds_cluster_parameter_group.cluster_pg]
+  database_name                 = "udacityc2"
+  master_username               = "udacity"
+  master_password               = "MyUdacityPassword"
+  vpc_security_group_ids        = [aws_security_group.db_sg_1.id]
+  db_subnet_group_name          = aws_db_subnet_group.udacity_db_subnet_group.name
+  engine                        = "aurora"
+  engine_mode                   = "provisioned"
+  engine_version                = "5.6.mysql_aurora.1.19.1"
+  backup_retention_period       = 5
+  skip_final_snapshot           = true
+  storage_encrypted             = false
+  depends_on                    = [aws_rds_cluster_parameter_group.cluster_pg]
 }
 
 output "db_cluster_arn" {
   value = aws_rds_cluster.udacity_cluster.arn
 }
 
-output "db_instance_arn" {
-  value = aws_rds_cluster_instance.udacity_instance[0].arn
-}
-
 resource "aws_rds_cluster_instance" "udacity_instance" {
-  count                = 1
+  count                = 2
   identifier           = "udacity-db-instance-${count.index}"
   cluster_identifier   = aws_rds_cluster.udacity_cluster.id
   instance_class       = "db.t2.small"
@@ -67,7 +138,7 @@ resource "aws_rds_cluster_instance" "udacity_instance" {
 
 resource "aws_security_group" "db_sg_1" {
   name   = "udacity-db-sg"
-  vpc_id =  var.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   ingress {
     from_port   = 3306
